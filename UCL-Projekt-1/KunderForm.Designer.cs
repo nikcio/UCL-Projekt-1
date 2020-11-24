51dee5db22d4a5c8592317995ea2d4ec1ada2fd1
Nikcio <nikolajgive@gmail.com>
2020-11-24 10:26:16 +0100

    Formborder

--- UCL-Projekt-1/KunderForm.Designer.cs
+++ UCL-Projekt-1/KunderForm.Designer.cs
@@ -28,9 +28,11 @@ namespace UCL_Projekt_1 {
             // 
             // KunderForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "KunderForm";
             this.Text = "KunderForm";
             this.ResumeLayout(false);
--- UCL-Projekt-1/PersonaleForm.Designer.cs
+++ UCL-Projekt-1/PersonaleForm.Designer.cs
@@ -28,9 +28,11 @@ namespace UCL_Projekt_1 {
             // 
             // PersonaleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PersonaleForm";
             this.Text = "PersonaleForm";
             this.ResumeLayout(false);
