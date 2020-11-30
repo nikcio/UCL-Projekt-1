a7d2023aa420782b4a70f72c27cf06f38907c926
Nikcio <nikolajgive@gmail.com>
2020-11-30 10:06:48 +0100

    Nye forms

---
+++ UCL-Projekt-1/MæglerForm.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace UCL_Projekt_1 {
+    partial class MæglerForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // MæglerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MæglerForm";
+            this.Text = "MæglerForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
